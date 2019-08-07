902f3556eea69d2b2229bb9c1898d68ba3095e0a
Mateo <codeshader@gmail.com>
2019-08-07 22:42:47 +0200

    adds materials picker and fixes gui loading

--- app.js
+++ app.js
@@ -20,19 +20,22 @@ function init(){
 	window.addEventListener('resize', onWindowResize, false);
 	
 	guiData = {
+		BackgroundColor:'#1a2224',
 		currentURL:'models/Mjolnir.obj',
-		wireframe: false
+		material: 'Basic'
+		
 	};
+
 	console.log("INIT");
 	
 	loadModel(guiData.currentURL)
-	createGui();
+	// createGui(); Not needed since gui is creating after assigning a material
 }
 
 function loadModel(modelURL){
 	
 	scene = new THREE.Scene();
-	scene.background = new THREE.Color( 0x1a2224 );
+	scene.background = guiData.BackgroundColor;
 	let grid = new THREE.GridHelper(10, 10);
 	let axesHelper = new THREE.AxesHelper(3, 4);
 	scene.add(grid);
@@ -40,14 +43,13 @@ function loadModel(modelURL){
 	
 	//LIGHTS
 	let light = new THREE.PointLight( 0x1FE0FF, 2, 80 );
-
-	light.position.set( 30, 70, 0 );
-	scene.add( light );
 	let ambiLight = new THREE.AmbientLight( 0x404040, 3 ); // soft white light
+	light.position.set( 30, 70, 0 );
+	scene.add(light);
 	scene.add(light);
 	scene.add(ambiLight);
 
-	// //LOAD MANAGER FOR LOADER
+	//LOAD MANAGER FOR LOADER
 	const manager = new THREE.LoadingManager();
 	const load = document.getElementById("loading-screen");
 	const error = document.getElementById("error");
@@ -73,50 +75,39 @@ function loadModel(modelURL){
 	//LOAD MODEL
 	let loader = new THREE.OBJLoader(manager);
 	loader.load(modelURL, function ( object ) {
-		// object.traverse( function ( node ) {
 
-		// 	if ( node.isMesh ){
-				
-		// 		node.material = material;
-		// 		if (material.wireframe) node.material.wireframe = true;
-		// 	} 
-			
-		
-		//   } );
-		let material = new THREE.MeshLambertMaterial( { color: 0xffffff, wireframe:guiData.wireframe} );
+		let material = new THREE.MeshLambertMaterial( { color: 0xffffff} );
 
 		object.traverse( function ( child ) {
-
 			if ( child instanceof THREE.Mesh ) {
-	
 				child.material = material;
-	
 			}});
+
 		scene.add( object );
+		changeMaterial();
 		console.log(object);
+	
 
 	},
+
 	// called when loading is in progresses
 	function ( xhr ) {
 		let loadPercentage = xhr.loaded / xhr.total * 100;
 		console.log( loadPercentage + '% loaded' );
-
 	},
+
 	// called when loading has errors
 	function ( error ) {
-
 		console.log( 'An error happened' );
-
 	});
+
 	console.log("LOAD MODEL UPDATE");
-	
 }
 
 
 function onWindowResize(){
 	camera.aspect = container.offsetWidth / container.offsetHeight;
 	camera.updateProjectionMatrix();
-	
 	renderer.setSize(container.offsetWidth, container.offsetHeight);
 }
 
@@ -127,9 +118,11 @@ function animate(){
 };
 
 function createGui(){
-
+	console.log('GUI');
 	if (gui) gui.destroy();
-	gui = new dat.GUI({width:350});
+	gui = new dat.GUI({width:400});
+	
+	gui.addColor(guiData, 'BackgroundColor' ).onChange((val) =>{scene.background = new THREE.Color(val)});
 
 	gui.add(guiData, 'currentURL', {
 		"Mjolnir":'models/Mjolnir.obj',
@@ -140,28 +133,56 @@ function createGui(){
 
 	}).name('Model').onChange(update);
 	
-	gui.add(guiData, 'wireframe').onChange(updateWireframe);
-
-	function update(){
-		loadModel(guiData.currentURL);
-	}
-	
+	gui.add(guiData, 'material', ['Basic', 'Wireframe', 'Glossy']).onChange(changeMaterial);
 	
 }
 
-function updateWireframe(){
-	let material = new THREE.MeshLambertMaterial( {wireframe:guiData.wireframe} );
-	scene.traverse( function ( node ) {
-		
-		if ( node.isMesh ){
-			
-			node.material = material;
-			if (material.wireframe) node.material.wireframe = true;
-		} 
-		
+function update(){
+	loadModel(guiData.currentURL);
+}
+
+function changeMaterial(){
+	phongData = {
+		MeshColor:'#e5e5e5',
+		Specular: "#009900",
+		Shininess: 30,
+		FlatShading: false
+	}
+	if (guiData.material == 'Glossy'){
+		createGui();
+		let material = new THREE.MeshPhongMaterial( { color: 0xe5e5e5, specular: 0x009900, shininess: 30} );
+		assignMaterial(material);
+		let props = gui.addFolder('Phong');
+		props.addColor(phongData, 'MeshColor').onChange(val => material.color = new THREE.Color(val));
+		props.addColor(phongData, 'Specular').onChange(val => material.specular = new THREE.Color(val));
+		props.add(phongData, 'Shininess', 0, 100).onChange(val => material.shininess = val);
+		props.add(phongData, 'FlatShading').onChange(val => {material.flatShading = val; material.needsUpdate = true});
+		props.open();
+		console.log("PHONG UPDATE");
+	}
 	
-	  } );
-	  console.log("WIREFRAME UPDATE");
+	if (guiData.material == 'Basic'){
+		createGui();
+		let material = new THREE.MeshLambertMaterial();
+		assignMaterial(material);
+		console.log("LAMBERT UPDATE");
+	}
+
+	if (guiData.material == 'Wireframe'){
+		createGui();
+		let material = new THREE.MeshBasicMaterial( {wireframe:true} );
+		gui.addColor(phongData, 'MeshColor').name('Wireframe Color').onChange(val => material.color = new THREE.Color(val));
+		assignMaterial(material);
+		console.log("WIREFRAME UPDATE");
+	}
+}
+
+function assignMaterial(material){
+	scene.traverse( function ( node ) {		
+		if ( node.isMesh ){
+			node.material = material;
+		} 
+	} );
 }
 
 function render(){
