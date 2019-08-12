6016d36bfae80724d08cb4b26dc4d7e5185a59ed
Mateo <codeshader@gmail.com>
2019-08-12 23:37:06 +0200

    adds basic file read

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const app = express();
 app.use(express.static(path.join(__dirname + '/public')));
 const router = express.Router();
@@ -9,3 +10,20 @@ router.get('/', (req, res)=>{res.sendFile(path.join(__dirname + '/views/index.ht
 //add the router
 app.use('/', router);
 app.listen(process.env.port || port, console.log(`Model viewer running on port ${port}!`));
+let model = [];
+const directoryPath = path.join(__dirname, "/public/models");
+fs.readdir(directoryPath, function (err, files){
+    //handling error
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    } 
+    //listing all files using forEach
+    files.forEach(function (file) {
+    model.push(file  + " : " + "models/" + file);
+        // Do whatever you want to do with the file 
+    });
+    console.log(model);
+
+});
+
+exports.model = model;
