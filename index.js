f65433d96504513691aae4558b049d7351966f38
djukela17 <gracin.julian@gmail.com>
2019-08-13 09:35:16 +0200

    add model list endpoint api

--- index.js
+++ index.js
@@ -1,29 +1,47 @@
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
 const app = express();
-app.use(express.static(path.join(__dirname + '/public')));
+app.use(express.static(path.join(__dirname + "/public")));
 const router = express.Router();
 const port = 5500;
 
-router.get('/', (req, res)=>{res.sendFile(path.join(__dirname + '/views/index.html'))});
-//add the router
-app.use('/', router);
-app.listen(process.env.port || port, console.log(`Model viewer running on port ${port}!`));
-let model = [];
-const directoryPath = path.join(__dirname, "/public/models");
-fs.readdir(directoryPath, function (err, files){
-    //handling error
-    if (err) {
-        return console.log('Unable to scan directory: ' + err);
-    } 
-    //listing all files using forEach
-    files.forEach(function (file) {
-    model.push(file  + " : " + "models/" + file);
-        // Do whatever you want to do with the file 
-    });
-    console.log(model);
-
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/views/index.html"));
 });
 
-exports.model = model;
+router.get("/model_list", (req, res) => {
+  let data = {
+      models: [],
+  };
+  const directoryPath = path.join(__dirname, "/public/models");
+
+  fs.readdir(directoryPath, (err, files) => {
+    //handling error
+    if (err) {
+      return console.log("Unable to scan directory: " + err);
+    }
+    //listing all files using forEach
+    files.forEach( file => {
+      // Do whatever you want to do with the file
+      const name = file.split('.')[0]
+        const newModel = {
+            name,
+            path: "models/" + file,
+        }
+        data.models.push(newModel)
+    });
+    console.log(data);
+    res.set("Content-Type", "Application/JSON")
+
+    res.send(data)
+  });
+});
+
+//add the router
+app.use("/", router);
+
+app.listen(
+  process.env.port || port,
+  console.log(`Model viewer running on port ${port}!`)
+);
